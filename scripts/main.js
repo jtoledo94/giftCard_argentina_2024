41db789151e739b3cac2ecb3c89d797fec374394
jtoledo94 <jtoledo@coloniaexpress.com>
2025-05-30 12:37:09 -0300

    feat: add link to whats app

--- scripts/main.js
+++ scripts/main.js
@@ -29,23 +29,23 @@ lista = [
       {
         texto: "Menú de 3 pasos o pic-nic entre las viñas a su elección",
         icon: "fa fa-cutlery pe-2 azul"
-      },
-      {
-        texto: "Lunes a domingos",
-        icon: "fa fa-calendar azul  pe-2"
-      },
-      {
-        texto: "ida: 08:30 / vuelta: 18:00",
-        icon: "fa fa-clock-o  azul pe-2"
-      },
-      {
-        texto: "ida: 10:30 / vuelta: 20:30",
-        icon: "fa fa-clock-o  azul pe-2"
-      },
-      {
-        texto: "Canjeá tu regalo llamando al Call Center +54 11 5167 7700, comunicándote vía WhatsApp +54 9 11 6411-9465 o acercándote a nuestros puntos de venta",
-        icon: "fa-solid fa-gift pe-2 "
       }
+      /*  {
+         texto: "Lunes a domingos",
+         icon: "fa fa-calendar azul  pe-2"
+       },
+       {
+         texto: "ida: 08:30 / vuelta: 18:00",
+         icon: "fa fa-clock-o  azul pe-2"
+       },
+       {
+         texto: "ida: 10:30 / vuelta: 20:30",
+         icon: "fa fa-clock-o  azul pe-2"
+       },
+       {
+         texto: "Canjeá tu regalo llamando al Call Center +54 11 5167 7700, comunicándote vía WhatsApp +54 9 11 6411-9465 o acercándote a nuestros puntos de venta",
+         icon: "fa-solid fa-gift pe-2 "
+       } */
 
     ],
     modalImgs: [
@@ -61,6 +61,26 @@ lista = [
       {
         imgC: "../img/wine/reel/wine1.webp"
       },
+    ],
+    modalfooter: [
+
+      {
+        texto: "Lunes a domingos",
+        icon: "fa fa-calendar azul  pe-2"
+      },
+      {
+        texto: "ida: 08:30 / vuelta: 18:00",
+        icon: "fa fa-clock-o  azul pe-2"
+      },
+      {
+        texto: "ida: 10:30 / vuelta: 20:30",
+        icon: "fa fa-clock-o  azul pe-2"
+      },
+      {
+        texto: "Canjeá tu regalo llamando al Call Center +54 11 5167 7700, comunicándote vía WhatsApp [whatsapp] o acercándote a nuestros puntos de venta"
+,
+        icon: "fa-solid fa-gift pe-2 "
+      }
     ]
 
   },
@@ -92,23 +112,8 @@ lista = [
         texto: "Llevá tu bicicleta gratis",
         icon: "fa fa-bicycle azul pe-2"
       },
-  
-      {
-        texto: "Miércoles a Lunes",
-        icon: "fa fa-calendar azul  pe-2"
-      },
-      {
-        texto: "Ida: 08:30 / Vuelta: 18:00",
-        icon: "fa fa-clock-o  azul pe-2"
-      },
-      {
-        texto: "Ida: 10:30 / Vuelta: 20:30",
-        icon: "fa fa-clock-o  azul pe-2"
-      },
-      {
-        texto: "Canjeá tu regalo llamando al Call Center +54 11 5167 7700, comunicándote vía WhatsApp +54 9 11 6411-9465 o acercándote a nuestros puntos de venta",
-        icon: "fa-solid fa-gift pe-2 "
-      }
+
+
 
     ],
     modalImgs: [
@@ -124,6 +129,24 @@ lista = [
       {
         imgC: "../img/premium/reel/premium4.webp"
       },
+    ], modalfooter: [
+
+      {
+        texto: "Miércoles a Lunes",
+        icon: "fa fa-calendar azul  pe-2"
+      },
+      {
+        texto: "ida: 08:30 / vuelta: 18:00",
+        icon: "fa fa-clock-o  azul pe-2"
+      },
+      {
+        texto: "ida: 10:30 / vuelta: 20:30",
+        icon: "fa fa-clock-o  azul pe-2"
+      },
+      {
+        texto: "Canjeá tu regalo llamando al Call Center +54 11 5167 7700, comunicándote vía WhatsApp [whatsapp] o acercándote a nuestros puntos de venta",
+        icon: "fa-solid fa-gift pe-2 "
+      }
     ]
 
   },
@@ -155,7 +178,7 @@ lista = [
         texto: "Llevá tu bicicleta gratis",
         icon: "fa fa-bicycle azul pe-2"
       },
-  
+
       {
         texto: "Miércoles a Lunes",
         icon: "fa fa-calendar azul  pe-2"
@@ -169,7 +192,7 @@ lista = [
         icon: "fa fa-clock-o  azul pe-2"
       },
       {
-        texto: "Canjeá tu regalo llamando al Call Center +54 11 5167 7700, comunicándote vía WhatsApp +54 9 11 6411-9465 o acercándote a nuestros puntos de venta",
+        texto: "Canjeá tu regalo llamando al Call Center +54 11 5167 7700, comunicándote vía WhatsApp [whatsapp] o acercándote a nuestros puntos de venta",
         icon: "fa-solid fa-gift pe-2 "
       }
 
@@ -187,7 +210,72 @@ lista = [
       {
         imgC: "../img/premium/reel/premium4.webp"
       },
-    ]
+    ],
+
+
+  },
+  {
+    id: 4,
+    upperTitle: "DAY TOUR",
+    title: "PREMIUM",
+    img: "../img/premium/premium.webp",
+    modalId: "premium",
+    indicatiors: 4,
+    modal: [
+      {
+        texto: "Pasaje ida y vuelta",
+        icon: "fa-solid fa-ship azul pe-2"
+      },
+      {
+        texto: "Paseo guiado por el Barrio Histórico",
+        icon: "fa fa-street-view azul pe-2"
+      },
+      {
+        texto: "Almuerzo en Mesón de la Plaza c/bebidas",
+        icon: "fa fa-cutlery azul pe-2"
+      },
+      {
+        texto: "Tiempo Libre en la ciudad",
+        icon: "fa fa-hourglass-half azul pe-2"
+      },
+      {
+        texto: "Llevá tu bicicleta gratis",
+        icon: "fa fa-bicycle azul pe-2"
+      },
+
+      {
+        texto: "Miércoles a Lunes",
+        icon: "fa fa-calendar azul  pe-2"
+      },
+      {
+        texto: "Ida: 08:30 / Vuelta: 18:00",
+        icon: "fa fa-clock-o  azul pe-2"
+      },
+      {
+        texto: "Ida: 10:30 / Vuelta: 20:30",
+        icon: "fa fa-clock-o  azul pe-2"
+      },
+      {
+        texto: "Canjeá tu regalo llamando al Call Center +54 11 5167 7700, comunicándote vía WhatsApp [whatsapp] o acercándote a nuestros puntos de venta",
+        icon: "fa-solid fa-gift pe-2 "
+      }
+
+    ],
+    modalImgs: [
+      {
+        imgC: "../img/premium/reel/premium1.webp"
+      },
+      {
+        imgC: "../img/premium/reel/premium2.webp"
+      },
+      {
+        imgC: "../img/premium/reel/premium3.webp"
+      },
+      {
+        imgC: "../img/premium/reel/premium4.webp"
+      },
+    ],
+
 
   },
 ]
@@ -318,14 +406,27 @@ const crearModal = (list) => {
       });
 
       // SECCIÓN ADICIONAL (si querés manejar horarios, días, etc.)
-      if (element.footer) {
+      if (element.modalfooter) {
         const divFooter = document.createElement("div");
         divFooter.classList.add("container", "contenedor_modal_texto");
-        element.footer.forEach(linea => {
-          const p = document.createElement("p");
-          p.classList.add("poppins-regular", "bordo");
-          p.innerHTML = `<i class="${linea.icon}"></i>${linea.texto}`;
-          divFooter.appendChild(p);
+        element.modalfooter.forEach(linea => {
+          if (linea.icon.includes("fa-gift")) {
+            const p = document.createElement("p");
+            p.classList.add("poppins-regular", "black");
+
+            const whatsappLink = '<a href="https://wa.link/ujxlug" target="_blank"  text-decoration:none;">+54 9 11 6411-9465</a>';
+            const textoTransformado = linea.texto.replace('[whatsapp]', whatsappLink);
+
+            p.innerHTML = `<i class="${linea.icon}"></i>${textoTransformado}`;
+            divFooter.appendChild(p);
+          }
+          else {
+
+            const p = document.createElement("p");
+            p.classList.add("poppins-regular", "bordo");
+            p.innerHTML = `<i class="${linea.icon}"></i>${linea.texto}`;
+            divFooter.appendChild(p);
+          }
         });
         sectionTexto.appendChild(divFooter);
       }
@@ -338,104 +439,7 @@ const crearModal = (list) => {
 
 
 
-//MODALES
-{/* <section>
-      <div class="modal fade" id="premium" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header d-none">
 
-
-
-            </div>
-            <div class="modal-body">
-
-              <!--CARRUSEL-->
-              <section>
-
-                <div id="carouselExampleIndicators2" class="carousel slide position-relative">
-                  <button type="button" class="btn-close position-absolute" data-bs-dismiss="modal"
-                    aria-label="Close"></button>
-                  <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="0"
-                      class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="1"
-                      aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="2"
-                      aria-label="Slide 3"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="3"
-                      aria-label="Slide 4"></button>
-                  </div>
-                  <div class="carousel-inner">
-
-                    <div class="carousel-item active carrusel_contenedor_imagenes">
-                      <img src="img/premium/reel/premium1.webp" class="d-block img_carrito_modal"
-                        alt="Day Tour - Premium en Colonia Express">
-                    </div>
-                    <div class="carousel-item carrusel_contenedor_imagenes">
-                      <img src="img/premium/reel/premium2.webp" class="d-block img_carrito_modal"
-                        alt="Day Tour - Premium en Colonia Express">
-                    </div>
-                    <div class="carousel-item carrusel_contenedor_imagenes">
-                      <img src="img/premium/reel/premium3.webp" class="d-block img_carrito_modal"
-                        alt="Day Tour - Premium en Colonia Express">
-                    </div>
-                    <div class="carousel-item carrusel_contenedor_imagenes">
-                      <img src="img/premium/reel/premium4.webp" class="d-block img_carrito_modal"
-                        alt="Day Tour - Premium en Colonia Express">
-                    </div>
-
-
-                  </div>
-                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators2"
-                    data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                  </button>
-                  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators2"
-                    data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                  </button>
-                </div>
-              </section>
-              <!--TEXTO-->
-              <section class="mt-3 seccion_texto_modal">
-
-                <div class="contenedor_modal_texto container wine_experience_contendor">
-                  <h2 class="poppins-extrabold azul ">PREMIUM</h2>
-
-                  <p class="poppins-regular"><i class="fa-solid fa-ship azul pe-2"></i>Pasaje ida y vuelta</p>
-                  <p class="poppins-regular"><i class="fa fa-street-view azul pe-2"></i>Paseo guiado por el Barrio
-                    Histórico</p>
-                  <p class="poppins-regular"><i class="fa fa-cutlery azul pe-2"></i>Almuerzo en Mesón de la Plaza
-                    c/bebidas</p>
-                  <p class="poppins-regular"><i class="fa fa-hourglass-half azul pe-2"></i>Tiempo Libre en la ciudad</p>
-                  <p class="poppins-regular"><i class="fa fa-bicycle azul pe-2"></i>Llevá tu bicicleta gratis</p>
-                </div>
-
-                <div class="container contenedor_modal_texto">
-                  <p class="poppins-regular bordo"><i class="fa fa-calendar azul pe-2"></i>Miércoles a Lunes.</p>
-                  <p class="poppins-regular bordo"><i class="fa fa-clock-o azul pe-2"></i>Ida: 08:30 / Vuelta: 18:00
-                  </p>
-                  <p class="poppins-regular bordo"><i class="fa fa-clock-o azul pe-2"></i>Ida: 10:30 / Vuelta: 20:30
-                  </p>
-
-                  <p class="poppins-regular p_footer_modal"><i class="fa-solid fa-gift pe-2 "></i>Canjeá tu regalo
-                    llamando al Call Center +54 11 5167 7700, comunicándote vía WhatsApp <a target="_blank"
-                      href="https://wa.link/ujxlug">+54 9 11 6411-9465</a> o acercándote a nuestros puntos de
-                    venta
-                </div>
-
-
-              </section>
-
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </section> */}
 
 
 
@@ -497,11 +501,11 @@ const crearCards = (list) => {
       title.innerText = `${element.title}`;
       cardBody.appendChild(title);
 
-      
+
 
       const button = document.createElement("button");
       button.type = "button"
-      button.classList.add("btn", "btn-primary", "btn_card", "poppins-semibold", "celeste","order-3")
+      button.classList.add("btn", "btn-primary", "btn_card", "poppins-semibold", "celeste", "order-3")
       button.setAttribute("data-bs-toggle", "modal")
       button.setAttribute("data-bs-target", `#${element.modalId}`);
       button.innerText = "Ver más"
